5c5ac9514cc18473185e15daa4ebdedd10aae7b6
Ravi9896 <128831946+Ravi9896@users.noreply.github.com>
2023-12-30 11:10:03 +0530

    adding logic to button

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+  const [disabledSub, setDisabledSub] = useState(false);
+  const value = count;
+
+  const Add = () => {
+    //codtionally change the value of disabled
+    if (count >= 0) {
+      setDisabledSub(false);
+    }
+    //counter logic part
+    if (count >= 10) {
+      setDisabled(true);
+    } else {
+      setCount(count + 1);
+    }
+  };
+  const Sub = () => {
+    //codtionally change the value of disabled
+    if (count <= 10) {
+      setDisabled(false);
+    }
+    //counter logic part
+    if (count <= 0) {
+      setDisabledSub(true);
+    } else {
+      setCount(count - 1);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter Value = {value}</h1>
+      <button
+        onClick={Add}
+        disabled={disabled}
+     
+      >
+        Add
+      </button>{" "}
+      <button
+        onClick={Sub}
+        disabled={disabledSub}
+       
+      >
+        Sub
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
